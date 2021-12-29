849f75df4e5bf55ae62df293e9fb28b5a840ee4c
xiangpin <xshuangbin@163.com>
2021-12-29 22:02:45 +0800

    update linear layout

--- R/geom-curve-link.R
+++ R/geom-curve-link.R
@@ -7,7 +7,6 @@
 #'
 #' @inheritParams ggplot2::layer
 #' @inheritParams grid::curveGrob
-#' @param hratio numeric, control the link height, default is 0.5 .
 #' @param arrow specification for arrow heads, as created by arrow().
 #' @param arrow.fill fill colour to use for the arrow head (if closed). `NULL`
 #'        means use `colour` aesthetic.
@@ -18,13 +17,11 @@
 #' @importFrom ggplot2 layer
 #' @author Shuangbin Xu
 #' @export
-geom_curve_link <- function(mapping = NULL, data = NULL,
-                            stat = "identity", position = "identity",
+geom_curve_link <- function(mapping = NULL, 
+                            data = NULL,
+                            stat = "identity", 
+                            position = "identity",
                             ...,
-                            curvature = NA,
-                            angle = 90,
-                            ncp = 1,
-                            hratio = 0.5,
                             arrow = NULL,
                             arrow.fill = NULL,
                             lineend = "butt",
@@ -43,12 +40,8 @@ geom_curve_link <- function(mapping = NULL, data = NULL,
     params = list(
       arrow = arrow,
       arrow.fill = arrow.fill,
-      curvature = curvature,
-      angle = angle,
-      ncp = ncp,
       lineend = lineend,
       na.rm = na.rm,
-      hratio = hratio,
       ...
     )
   )
@@ -72,63 +65,66 @@ GeomCurveLink <- ggproto("GeomCurveLink", Geom,
     required_aes = c("x", "y", "xend", "yend"),
     non_missing_aes = c("linetype", "size", "shape"),
     optional_aes = c("subset"),
-    default_aes = aes(colour = "black", size = 0.3, linetype = 1, alpha = 0.4, curvature=NA),
+    default_aes = aes(colour = "black", size = 0.3, linetype = 1, alpha = 0.4, curvature=NA, curveangle=90, square=FALSE),
     draw_key = draw_key_path,
-
     setup_data = function(data, params){
         if (!is.null(data$subset)){
             data <- data[which(data$subset),,drop=FALSE]
         }
         data
     },
+    draw_panel = function(data, panel_params, coord, shape = 0.5, arrow = NULL,
+                          hratio = 1, ncp = 1, arrow.fill = NULL, lineend = "butt", 
+                          na.rm = FALSE) {
 
-    draw_panel = function(data, panel_params, coord, hratio = 0.5, #outward = TRUE,
-                          angle = 90, ncp = 1, arrow = NULL, arrow.fill = NULL, 
-                          lineend = "butt", na.rm = FALSE) {
-
-    if (!coord$is_linear()){
-        tmpgroup <- data$group
-        starts <- subset(data, select = c(-xend, -yend))
-        starts$group <- 1
-        ends <- rename(subset(data, select = c(-x, -y)), c("x" = "xend", "y" = "yend"))
-        ends$group <- 2
-        pieces <- rbind(starts, ends)
-        
-        trans <- coord$transform(pieces, panel_params)
-        starts <- trans[trans$group==1, ,drop=FALSE]
-        ends <- trans[trans$group==2, ,drop=FALSE]
-        if (all(is.na(trans$curvature))){
-            curvature <- unlist(mapply(build_curvature, 
-                                       starttheta=starts$theta, 
-                                       endtheta=ends$theta, 
-                                       MoreArgs=list(hratio=hratio, ncp=ncp),
-                                       SIMPLIFY=FALSE))
-        }
-        ends <- rename(subset(ends, select=c(x, y)), c("xend"="x", "yend"="y"))
-        trans <- cbind(starts, ends)
-        trans$group <- tmpgroup
-        trans$curvature <- curvature
-    }else{
-        trans <- coord$transform(data, panel_params)
-        if (all(is.na(trans$curvature))){
-            trans$curvature <- 0.5
-        }
+      if (!coord$is_linear()){
+          tmpgroup <- data$group
+          starts <- subset(data, select = c(-xend, -yend))
+          starts$group <- 1
+          ends <- rename(subset(data, select = c(-x, -y)), c("x" = "xend", "y" = "yend"))
+          ends$group <- 2
+          pieces <- rbind(starts, ends)
+          
+          trans <- coord$transform(pieces, panel_params)
+          starts <- trans[trans$group==1, ,drop=FALSE]
+          ends <- trans[trans$group==2, ,drop=FALSE]
+          if (all(is.na(trans$curvature))){
+              curvature <- unlist(mapply(build_curvature, 
+                                         starttheta=starts$theta, 
+                                         endtheta=ends$theta, 
+                                         MoreArgs=list(hratio = hratio, ncp = ncp),
+                                         SIMPLIFY=FALSE))
+          }else{
+              curvature <- trans$curvature
+          }
+          ends <- rename(subset(ends, select=c(x, y)), c("xend"="x", "yend"="y"))
+          trans <- cbind(starts, ends)
+          trans$group <- tmpgroup
+          trans$curvature <- curvature
+      }else{
+          trans <- coord$transform(data, panel_params)
+          logicflag <- trans$y < trans$yend
+          if (all(is.na(trans$curvature))){
+              trans$curvature <- ifelse(logicflag, -1, 1) * 0.5
+          }else{
+              trans$curvature <- ifelse(logicflag, -1, 1) * abs(trans$curvature)
+          }
+      }
+      arrow.fill <- arrow.fill %|||% trans$colour
+      grobs <- lapply(seq_len(nrow(trans)), function(i){
+                          curveGrob(
+                            trans$x[i], trans$y[i], trans$xend[i], trans$yend[i],
+                            default.units = "native",
+                            curvature = trans$curvature[i], angle = trans$curveangle[i], ncp = ncp,
+                            square = trans$square[i], squareShape = 1, inflect = FALSE, open = TRUE,
+                            gp = gpar(col = alpha(trans$colour[i], trans$alpha[i]),
+                                      fill = alpha(arrow.fill[i], trans$alpha[i]),
+                                      lwd = trans$size[i] * ggplot2::.pt,
+                                      lty = trans$linetype[i],
+                                      lineend = lineend),
+                            arrow = arrow)})
+      class(grobs) <- "gList"
+      ggname("geom_curve_link", gTree(children=grobs))
     }
-    arrow.fill <- arrow.fill %|||% trans$colour
-    grobs <- lapply(seq_len(nrow(trans)), function(i){
-                        curveGrob(
-                          trans$x[i], trans$y[i], trans$xend[i], trans$yend[i],
-                          default.units = "native",
-                          curvature = trans$curvature[i], angle = angle, ncp = ncp,
-                          square = FALSE, squareShape = 1, inflect = FALSE, open = TRUE,
-                          gp = gpar(col = alpha(trans$colour[i], trans$alpha[i]),
-                                    fill = alpha(arrow.fill[i], trans$alpha[i]),
-                                    lwd = trans$size[i] * ggplot2::.pt,
-                                    lty = trans$linetype[i],
-                                    lineend = lineend),
-                          arrow = arrow)})
-    class(grobs) <- "gList"
-    ggname("geom_curve_link", gTree(children=grobs))
-  }
 )
 
